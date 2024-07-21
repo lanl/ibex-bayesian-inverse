29a6e21ce5477ff5b2a5fde9e9b79faee317d684
Steven Barnett <sdbarnett@vt.edu>
2024-07-20 21:39:12 -0400

    shell script for no discrepancy calibration

---
+++ automations/ibex_calib_no_disc.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+#SBATCH -N 1 
+#SBATCH --ntasks-per-node=66
+#SBATCH -t 96:00:00
+#SBATCH -p normal_q
+#SBATCH -A ascclass
+
+module reset
+module load R/4.1.0-foss-2021a
+
+export OMP_NUM_THREADS=2
+
+echo "Started script"
+R CMD BATCH "--args -debug -v --procs=33 --nmcmcs=10000" calib.R
+echo "Finished calibration script"
