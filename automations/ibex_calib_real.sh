9c700be51674676311405ee8a9eb096bc21c5556
Steven Barnett <sdbarnett@vt.edu>
2024-07-24 14:12:50 -0400

    adding shell script for real data

---
+++ automations/ibex_calib_real.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+
+#SBATCH -N 1 
+#SBATCH --ntasks-per-node=8
+#SBATCH -t 96:00:00
+#SBATCH -p normal_q
+#SBATCH -A ascclass
+
+module reset
+module load R/4.1.0-foss-2021a
+
+export OMP_NUM_THREADS=2
+
+if ! [[ $1 =~ ^[0-9]+$ ]];
+then
+  echo "Argument 1 must be integer. Indicates number of Monte Carlo repetitions."
+  echo "Usage: ibex_calib_real.sh [MC reps] [year]"
+  exit 1
+fi
+
+
+if ! [[ $2 =~ ^20[0-2][0-9]A$ ]];
+then
+  echo "Argument 2 must be a year between 2009-2022."
+  echo "Indicates what year the real data should come from."
+  echo "Usage: ibex_calib_real.sh [MC reps] [year]"
+  exit 1
+fi
+
+echo "Started script"
+R CMD BATCH "--args -v --procs=1 --fyear=$2" calib.R
+echo "Finished calibration script"
