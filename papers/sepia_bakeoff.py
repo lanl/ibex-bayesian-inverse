3e7f0bc5c0513c0703b5cea97e5415e52db1a38f
Steven Barnett <sdbarnett@vt.edu>
2025-03-24 19:29:20 -0400

    polishing sepia tests

---
+++ papers/sepia_bakeoff.py
@@ -0,0 +1,55 @@
+import csv
+import numpy as np
+import time
+from sepia.SepiaModel import SepiaModel
+from sepia.SepiaData import SepiaData
+from sepia.SepiaPredict import SepiaEmulatorPrediction
+
+x = np.genfromtxt('calib_params_ibex.csv', delimiter=',') ## pmfp, ratio
+y = np.transpose(np.genfromtxt('ibex_responses.csv', delimiter=',')) ## 66 vectors of 16200
+yx = np.genfromtxt('lat_lon_grid.csv', delimiter=',') ## lat lon grid
+
+n_samp = 10000
+times = np.zeros((len(x), 4))
+preds = np.zeros((len(x), yx.shape[0]))
+stddevs = np.zeros((len(x), yx.shape[0]))
+for i in range(len(x)):
+    yy_true = y[[i],:]
+    inds = np.delete(range(len(x)), i)
+    data = SepiaData(x_sim = x[inds,], y_sim = y[inds,], y_ind_sim = yx)
+    data.transform_xt()
+    data.standardize_y()
+    data.create_K_basis(n_pc=0.99)
+    model = SepiaModel(data)
+    model.verbose = False
+    tic = time.time()
+    model.tune_step_sizes(50, 20, verbose=False)
+    print("\n")
+    model.do_mcmc(n_samp)
+    toc = time.time()
+    times[i,0] = toc - tic
+    xx = x[[i], :]
+    n_pred=xx.shape[0]
+    pred_samples = model.get_samples(nburn=int(.1*n_samp),effectivesamples=True)
+    tic = time.time()
+    pred = SepiaEmulatorPrediction(x_pred=xx, samples=pred_samples, model=model,
+       storeMuSigma=True)
+    toc = time.time()
+    times[i,1] = toc - tic
+    predy = pred.get_y()
+    preds[i,] = np.mean(predy,0)
+    stddevs[i,] = np.std(predy,0)
+    print("\n")
+    print("Finished iteration " + str(i) + "\n")
+
+with open('sepia_times.csv', 'w', newline='') as file:
+    # Create a CSV writer object
+    writer = csv.writer(file)  
+    # Write each row of data to the CSV file
+    writer.writerows(times)
+
+with open('sepia_preds.csv', 'w', newline='') as file:
+    # Create a CSV writer object
+    writer = csv.writer(file)  
+    # Write each row of data to the CSV file
+    writer.writerows(preds)
---
+++ papers/sepia_time_test.py
@@ -0,0 +1,61 @@
+import csv
+import math
+import numpy as np
+from random import sample
+import time
+from sepia.SepiaModel import SepiaModel
+from sepia.SepiaData import SepiaData
+from sepia.SepiaPredict import SepiaEmulatorPrediction
+
+x = np.genfromtxt('calib_params_ibex.csv', delimiter=',') ## pmfp, ratio
+y = np.transpose(np.genfromtxt('ibex_responses.csv', delimiter=',')) ## 66 vectors of 16200
+yx = np.genfromtxt('lat_lon_grid.csv', delimiter=',') ## lat lon grid
+
+exp_pows = range(7, 45, 1)
+mcs = 5
+fit_times = np.zeros((mcs, len(exp_pows)))
+pred_times = np.zeros((mcs, len(exp_pows)))
+n_samp = 10000
+
+for i in range(len(exp_pows)):
+    ## select training data size
+    n = int(round(10 + math.pow(1.25, exp_pows[i]), 0))
+    print("n = 1.25^" + str(exp_pows[i]) + " = " + str(n))
+    for j in range(mcs):
+        sim_num = sample(range(66), 1)
+        non_sim_nums = np.delete(range(66), sim_num)
+        inds = sample(range(16200), n)
+        y_iter = y[non_sim_nums,]
+        y_iter = y_iter[:,inds]
+        data = SepiaData(x_sim = x[non_sim_nums,], y_sim = y_iter, y_ind_sim = yx[inds,])
+        data.transform_xt()
+        data.standardize_y()
+        data.create_K_basis(n_pc=0.99)
+        model = SepiaModel(data)
+        model.verbose = False
+        tic = time.time()
+        model.tune_step_sizes(50, 20, verbose=False)
+        model.do_mcmc(n_samp)
+        toc = time.time()
+        fit_times[j,i] = toc - tic
+        xx = x[sim_num, :]
+        n_pred=xx.shape[0]
+        pred_samples = model.get_samples(nburn=int(.1*n_samp),effectivesamples=True)
+        tic = time.time()
+        pred = SepiaEmulatorPrediction(x_pred=xx, samples=pred_samples, model=model,
+           storeMuSigma=True)
+        toc = time.time()
+        pred_times[j,i] = toc - tic
+        print("Finished Monte Carlo iteration " + str(j+1) + "/5 of n=" + str(n))
+
+with open('sepia_fit_times.csv', 'w', newline='') as file:
+    # Create a CSV writer object
+    writer = csv.writer(file)  
+    # Write each row of data to the CSV file
+    writer.writerows(fit_times)
+
+with open('sepia_pred_times.csv', 'w', newline='') as file:
+    # Create a CSV writer object
+    writer = csv.writer(file)  
+    # Write each row of data to the CSV file
+    writer.writerows(pred_times)
