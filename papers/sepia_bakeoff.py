96eafb115572a163fc0951bbbb99123d61ea178b
Steven Barnett <sdbarnett@vt.edu>
2025-04-14 14:42:46 -0400

    adding crps calculation for sepia

--- papers/sepia_bakeoff.py
+++ papers/sepia_bakeoff.py
@@ -5,6 +5,7 @@ import csv
 import math
 import numpy as np
 import time
+from scipy.stats import norm
 from sepia.SepiaModel import SepiaModel
 from sepia.SepiaData import SepiaData
 from sepia.SepiaPredict import SepiaEmulatorPrediction
@@ -14,15 +15,12 @@ y = np.transpose(np.genfromtxt('ibex_responses.csv', delimiter=',', skip_header=
 yx = np.genfromtxt('xyz_grid.csv', delimiter=',', skip_header=1) ## xyz grid
 
 n_samp = 10000
-preds = np.zeros((len(x), yx.shape[0]))
-s2s = np.zeros((len(x), yx.shape[0]))
-rmses = np.zeros(len(x))
-crps = np.zeros(len(x))
+metrics = np.zeros((len(x), 2))
 
 for i in range(len(x)):
     yy_true = y[[i],:][0]
     inds = np.delete(range(len(x)), i)
-    data = SepiaData(x_sim = x[inds,], y_sim = y[inds,], y_ind_sim = yx)
+    data = SepiaData(x_sim=x[inds,], y_sim=y[inds,], y_ind_sim=yx)
     data.transform_xt()
     data.standardize_y()
     data.create_K_basis(n_pc=0.99)
@@ -38,23 +36,25 @@ for i in range(len(x)):
        storeMuSigma=True)
     predy = pred.get_y()
     meany = np.mean(predy,0)[0]
-    preds[i,] = np.mean(predy,0)
-    s2s[i,] = np.var(predy,0)
+    s2s = np.var(predy,0)[0]
     sumsq = 0
     for j in range(meany.shape[0]):
         sumsq += pow(meany[j] - yy_true[j], 2)
-    rmses[i] = math.pow(sumsq/meany.shape[0], 0.5)
+    metrics[i,0] = math.pow(sumsq/meany.shape[0], 0.5)
+    scores = np.zeros(yx.shape[0])
+    for j in range(meany.shape[0]):
+        sigma = pow(s2s[j], 0.5)
+        if (sigma == 0):
+            scores[j] = np.nan
+            continue
+        z = (yy_true[j] - meany[j])/sigma
+        scores[j] = sigma*(-1/(math.pow(math.pi, 0.5)) + 2*norm.pdf(z) + z*(2*norm.cdf(z)-1))
+    metrics[i,1] = np.nanmean(scores)
     print("\n")
     print("Finished iteration " + str(i) + "\n")
 
-with open('sepia_preds.csv', 'w', newline='') as file:
+with open('sepia_metrics.csv', 'w', newline='') as file:
     # Create a CSV writer object
     writer = csv.writer(file)  
     # Write each row of data to the CSV file
-    writer.writerows(preds)
-
-with open('sepia_rmses.csv', 'w', newline='') as file:
-    # Create a CSV writer object
-    writer = csv.writer(file)  
-    # Write each row of data to the CSV file
-    writer.writerow(rmses)
+    writer.writerows(metrics)
