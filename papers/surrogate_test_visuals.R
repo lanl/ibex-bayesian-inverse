f7e9d9e6ab1b607f0a24ae61921fa0dd777437b7
Steven Barnett <sdbarnett@vt.edu>
2025-08-11 15:43:57 -0400

    code to produce rmse and crps plots

--- papers/surrogate_test_visuals.R
+++ papers/surrogate_test_visuals.R
@@ -1,41 +1,33 @@
-res <- readRDS("surrogate_test_202050801.rds")
+surr1_res <- readRDS("surrogate_test_20250811.rds")
+sepia_files <- list.files(pattern="sepia_metrics_[3-6].csv")
+
+rmses <- surr1_res$rmse
+crps <- surr1_res$crps
+for (i in 1:length(sepia_files)) {
+  sep_res <- read.csv(sepia_files[i], header=FALSE)
+  rmses <- cbind(rmses, sep_res[,1])
+  crps <- cbind(crps, sep_res[,2])
+  colnames(crps)[ncol(crps)] <- colnames(rmses)[ncol(rmses)] <- paste0("sepia", i+2)
+}
 
 par(mfrow=c(1,1), mar=c(5.1, 4.1, 0.2, 0.2))
-pdf("ibex_surr_rmse1.pdf", width=4, height=5)
-rmse1 <- res$rmse[,c(1,5,6,7)]
-labels <- c("SVEC (m=25)", "laGP", "deepgp", "SEPIA (pc=3)")
-boxplot(rmse1, ylab="rmse", xaxt="n")
-axis(1, at=1:ncol(rmse1), labels = FALSE)
-text(x=1:ncol(rmse1), y=par("usr")[3]-0.0003, labels=labels, srt=45,
+pdf("ibex_surr_rmse.pdf", width=7, height=5)
+rmses_ord <- rmses[,c(1,5:7,2:4,8:10)]
+labels <- c("SVEC (m=25)", "laGP", "deepgp", "SEPIA (pc=3)",
+  paste0("SVEC (m=", c(50, 75, 100), ")"), paste0("SEPIA (pc=", 4:6, ")"))
+boxplot(rmses_ord, ylab="rmse", xaxt="n")
+axis(1, at=1:ncol(rmses_ord), labels = FALSE)
+text(x=1:ncol(rmses_ord), y=par("usr")[3]-0.0003, labels=labels, srt=45,
   xpd=TRUE, adj=1, cex=0.9)
+abline(v=4.5, lty=2)
 dev.off()
 
 par(mfrow=c(1,1), mar=c(5.1, 4.1, 0.2, 0.2))
-pdf("ibex_surr_rmse2.pdf", width=4, height=5)
-rmse2 <- res$rmse[,c(2:4, 8:10)]
-labels <- c("SVEC (m=50)", "SVEC (m=75)", "SVEC (m=100)", "SEPIA (pc=4)", "SEPIA (pc=5)", "SEPIA (pc=6)")
-boxplot(rmse2, ylab="rmse", xaxt="n")
-axis(1, at=1:ncol(rmse2), labels = FALSE)
-text(x=1:ncol(rmse2), y=par("usr")[3]-0.00003, labels=labels, srt=45,
+pdf("ibex_surr_crps.pdf", width=7, height=5)
+crps_ord <- crps[,c(1,5:7,2:4,8:10)]
+boxplot(crps_ord, ylab="crps", xaxt="n")
+axis(1, at=1:ncol(crps_ord), labels = FALSE)
+text(x=1:ncol(crps_ord), y=par("usr")[3]-0.0003, labels=labels, srt=45,
   xpd=TRUE, adj=1, cex=0.9)
-dev.off()
-
-par(mfrow=c(1,1), mar=c(5.1, 4.1, 0.2, 0.2))
-pdf("ibex_surr_rmse1.pdf", width=4, height=5)
-crps1 <- res$crps[,c(1,5,6,7)]
-labels <- c("SVEC (m=25)", "laGP", "deepgp", "SEPIA (pc=3)")
-boxplot(crps1, ylab="crps", xaxt="n")
-axis(1, at=1:ncol(crps1), labels = FALSE)
-text(x=1:ncol(crps1), y=par("usr")[3]-0.00015, labels=labels, srt=45,
-  xpd=TRUE, adj=1, cex=0.9)
-dev.off()
-
-par(mfrow=c(1,1), mar=c(5.1, 4.1, 0.2, 0.2))
-pdf("ibex_surr_crps2.pdf", width=4, height=5)
-crps2 <- res$crps[,c(2:4, 8:10)]
-labels <- c("SVEC (m=50)", "SVEC (m=75)", "SVEC (m=100)", "SEPIA (pc=4)", "SEPIA (pc=5)", "SEPIA (pc=6)")
-boxplot(crps2, ylab="crps", xaxt="n")
-axis(1, at=1:ncol(crps2), labels = FALSE)
-text(x=1:ncol(crps2), y=par("usr")[3]-0.000015, labels=labels, srt=45,
-  xpd=TRUE, adj=1, cex=0.9)
+abline(v=4.5, lty=2)
 dev.off()
