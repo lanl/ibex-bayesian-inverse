e5d61b42679bcd1c90a20b81fbcf92c6a3513d05
Steven Barnett <sdbarnett@vt.edu>
2025-08-27 15:14:39 -0400

    updating timing figure for number of runs

--- papers/surrogate_time_test_visuals.R
+++ papers/surrogate_time_test_visuals.R
@@ -51,14 +51,14 @@ dev.off()
 surr_times <- readRDS("surrogate_time_test_ns_20250806.rds")
 surr_fit_times <- apply(surr_times$fit_times, c(2,3), mean)
 surr_pred_times <- apply(surr_times$pred_times, c(2,3), mean)
-sepia_fit_files <- list.files(pattern="sepia_fit_times_3_ns.csv")
-sepia_pred_files <- list.files(pattern="sepia_pred_times_3_ns.csv")
+sepia_fit_files <- list.files(pattern="sepia_fit_times_[3-6]{1}_ns.csv")
+sepia_pred_files <- list.files(pattern="sepia_pred_times_[3-6]{1}_ns.csv")
 
 for (i in 1:length(sepia_fit_files)) {
   sep_fit <- read.csv(sepia_fit_files[i], header=FALSE)
   sep_pred <- read.csv(sepia_pred_files[i], header=FALSE)
-  surr_fit_times <- cbind(surr_fit_times, apply(sep_fit, 2, mean))
-  surr_pred_times <- cbind(surr_pred_times, apply(sep_pred, 2, mean))
+  surr_fit_times <- cbind(surr_fit_times, apply(sep_fit, 2, mean, na.rm=TRUE))
+  surr_pred_times <- cbind(surr_pred_times, apply(sep_pred, 2, mean, na.rm=TRUE))
   colnames(surr_fit_times)[ncol(surr_fit_times)] <-
     colnames(surr_pred_times)[ncol(surr_pred_times)] <- paste0("sepia", i+2)
 }
@@ -88,8 +88,12 @@ match_cols <- c(1,3:4)
 large_ns <- seq(500, 2500, by=500)
 par(mfrow=c(1,1), mar=c(5.1, 4.1, 0.2, 0.2))
 pdf("ibex_surr_total_times_ns.pdf", width=4, height=5)
-long_times_ord <- surr_total_times[,c(1,5,7)]
+long_times_ord <- surr_total_times[,c(1:4,7:10)]
 matplot(x=large_ns, y=long_times_ord[(length(ns)+1):nrow(long_times_ord),]/60, type="l",
   xlab="# simulator runs", ylab="fitting + prediction time (minutes)",
-  lwd=3, col=match_cols, lty=match_cols, ylim=c(0, 30))
+  lwd=3, col=rep(c(1,3), each=4), lty=rep(1:4, 2), ylim=c(0, 30))
+lines(x=large_ns, y=surr_total_times[(length(ns)+1):nrow(surr_total_times),5]/60,
+  lwd=3, col=4, lty=4)
+legend("topright", c("SVEC (m=25,50,75,100)", "SEPIA (pc=3,4,5,6)"), col=c(1,3),
+  lty=1, lwd=2, cex=0.75)
 dev.off()
