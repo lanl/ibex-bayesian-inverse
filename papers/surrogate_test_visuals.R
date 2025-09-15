39c1ac2b2b107f42dcd059ae392468d95750c631
Steven Barnett <sdbarnett@vt.edu>
2025-09-15 13:01:27 -0400

    small edits

--- papers/surrogate_test_visuals.R
+++ papers/surrogate_test_visuals.R
@@ -17,18 +17,18 @@ labels <- c("SVEC (m=25)", "laGP", "deepgp", expression(SEPIA~"("*n[k]*"="*3*")"
   paste0("SVEC (m=", c(50, 75, 100), ")"), expression(SEPIA~"("*n[k]*"="*4*")"),
   expression(SEPIA~"("*n[k]*"="*5*")"), expression(SEPIA~"("*n[k]*"="*6*")"))
 boxplot(rmses_ord, ylab="rmse", xaxt="n", cex.lab=1.05)
-axis(1, at=1:ncol(rmses_ord), labels=FALSE)
-text(x=1:ncol(rmses_ord), y=par("usr")[3]-0.0003, labels=labels, srt=45,
+axis(1, at=1:ncol(rmses_ord), labels=FALSE, tck=-0.02)
+text(x=1:ncol(rmses_ord), y=par("usr")[3]-0.0002, labels=labels, srt=45,
   xpd=TRUE, adj=1, cex=1.05)
 abline(v=4.5, lty=2)
 dev.off()
 
-par(mfrow=c(1,1), mar=c(5.1, 4.1, 0.2, 0.2))
+par(mfrow=c(1,1), mar=c(5.1, 4.3, 0.2, 0.2))
 pdf("ibex_surr_crps.pdf", width=7, height=5)
 crps_ord <- crps[,c(1,5:7,2:4,8:10)]
 boxplot(crps_ord, ylab="crps", xaxt="n", cex.lab=1.05)
-axis(1, at=1:ncol(crps_ord), labels = FALSE)
-text(x=1:ncol(crps_ord), y=par("usr")[3]-0.0003, labels=labels, srt=45,
+axis(1, at=1:ncol(crps_ord), labels = FALSE, tck=-0.02)
+text(x=1:ncol(crps_ord), y=par("usr")[3]-0.0002, labels=labels, srt=45,
   xpd=TRUE, adj=1, cex=1.05)
 abline(v=4.5, lty=2)
 dev.off()
