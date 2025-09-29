7761de7cd14bd99e76d54114a7a1545f11b322c5
Steven Barnett <sdbarnett@vt.edu>
2025-09-29 15:36:41 -0400

    adding collection script for cv experiment

--- automations/real_data_cv.sh
+++ automations/real_data_cv.sh
@@ -2,7 +2,7 @@
 
 #SBATCH -N 5
 #SBATCH --ntasks-per-node=20
-#SBATCH -t 4:00:00
+#SBATCH -t 24:00:00
 #SBATCH -p normal_q
 #SBATCH -A ibex
 
@@ -18,4 +18,13 @@ seed=$((RANDOM))
 nf="nodes_$(( RANDOM % 9000 + 1000)).txt"
 scontrol show hostname $SLURM_NODELIST > $nf
 
-seq 1 $ncopies | parallel --slf $nf -j$nparallel --wd $PWD --env OMP_NUM_THREADS "module reset; module load R/4.4.2-gfbf-2024a; R CMD BATCH \"--args seed=$seed ncvs=10 fid={} nmcmcs=200\" real_data_cv.R real_data_cv_{}.Rout"
+seq 1 $ncopies | parallel --slf $nf -j$nparallel --wd $PWD --env OMP_NUM_THREADS "module reset; module load R/4.4.2-gfbf-2024a; R CMD BATCH \"--args seed=$seed ncvs=10 fid={} nmcmcs=10000\" real_data_cv.R real_data_cv_{}.Rout"
+
+# Collect all the results
+R CMD BATCH "--args seed=$seed" real_data_cv_collect.R
+
+# Remove node file
+if [[ -f $nf ]];
+then
+  rm $nf
+fi
---
+++ automations/real_data_cv_collect.R
@@ -0,0 +1,101 @@
+library(scoringRules)
+
+source("../helper.R")
+source("../vecchia_scaled.R")
+
+seed <- 711930
+
+args <- commandArgs(TRUE)
+if (length(args) > 0) {
+  for (i in 1:length(args)) {
+    eval(parse(text=args[[i]]))
+  }
+}
+
+model_data <- read.csv(file="../data/sims.csv")
+field_data <- read.csv(file="../data/ibex_real.csv")
+field_data <- field_data[field_data$map %in% paste0(2009:2011, "A"),]
+field_data$xcod <- (field_data$x - min(field_data$x)) / diff(range(field_data$x))
+field_data$ycod <- (field_data$y - min(field_data$y)) / diff(range(field_data$y))
+field_data$zcod <- (field_data$z - min(field_data$z)) / diff(range(field_data$z))
+
+nf <- nrow(field_data)
+colnames(field_data)[colnames(field_data)== "counts"] <- "sim_counts"
+colnames(field_data)[colnames(field_data)== "ecliptic_lat"] <- "lat"
+colnames(field_data)[colnames(field_data)== "ecliptic_lon"] <- "lon"
+fparams <- paste0(2009:2011, "A")
+pd <- preprocess_data(md=model_data, fd=field_data, esa_lev=4,
+  fparams=fparams, scales=c(1, 1), tol=NA, quant=0.0, real=TRUE)
+
+files <- list.files(path="../results", pattern="real_data_cv_fold*")
+set.seed(seed)
+folds <- sample(1:nf, nf)
+folds <- split(folds, cut(seq_along(folds), length(files), labels=FALSE))
+
+### Fit Scaled GP Surrogate
+svecfit <- fit_scaled(y=pd$Zmod,
+  inputs=as.matrix(cbind(pd$Xmod, pd$Umod)), nug=1e-4, ms=25)
+
+post_means <- matrix(NA, ncol=2, nrow=length(files))
+colnames(post_means) <- c("pmfp", "ratio")
+
+sample_pmfps <- seq(500, 3000, length=500)
+sample_pmfps_cod <- (sample_pmfps - 500)/2500
+sample_ratios <- seq(0.001, 0.1, length=500)
+sample_ratios_cod <- (sample_ratios - 0.001)/(0.1-0.001)
+
+crps_pmfp <- matrix(NA, ncol=length(files), nrow=length(sample_pmfps))
+crps_ratio <- matrix(NA, ncol=length(files), nrow=length(sample_ratios))
+
+for (i in 1:length(files)) {
+  ### Read in results
+  iter_res <- readRDS(paste0("../results/", files[i]))
+  fold_id <- iter_res$settings$fid
+  field_fold <- field_data[folds[[fold_id]],]
+  Xfold <- field_fold[,c("xcod", "ycod", "zcod")]
+
+  ### Calculate posterior mean
+  post_mean_cod <- apply(iter_res$mcmc_res$u, 2, mean)
+  post_mean <- post_mean_cod
+  post_mean[1] <- post_mean_cod[1]*2500 + 500
+  post_mean[2] <- post_mean_cod[2]*(0.1-0.001) + 0.001
+  post_means[i,] <- post_mean
+
+  for (j in 1:length(sample_pmfps_cod)) {
+    ### Pull out of sample counts
+    Xfield <- cbind(Xfold, matrix(c(sample_pmfps_cod[j], post_mean_cod[2]), nrow=1))
+    colnames(Xfield) <- c("x", "y", "z", "pmfp", "ratio")
+
+    ### Predict lambda at out of sample using Scaled Vecchia
+    preds <- predictions_scaled(svecfit, as.matrix(Xfield), m=25,
+      joint=FALSE, predvar=FALSE)
+    ### Calculate CRPS (include time + background)
+    crps_pmfp[j,i] <- mean(crps_pois(y=field_fold$sim_counts,
+      lambda=preds*field_fold$time + field_fold$background))
+    if (j %% 20 == 0) {
+      print(paste0("Fold ", i, ": Finished pmfp ", j, "/", length(sample_pmfps)))
+    }
+  }
+
+  for (j in 1:length(sample_ratios_cod)) {
+    ### Pull out of sample counts
+    Xfield <- cbind(Xfold, matrix(c(post_mean_cod[1], sample_ratios_cod[j]), nrow=1))
+    colnames(Xfield) <- c("x", "y", "z", "pmfp", "ratio")
+
+    ### Predict lambda at out of sample using Scaled Vecchia
+    preds <- predictions_scaled(svecfit, as.matrix(Xfield), m=25,
+      joint=FALSE, predvar=FALSE)
+
+    ### Calculate CRPS (include time + background)
+    crps_ratio[j,i] <- mean(crps_pois(y=field_fold$sim_counts,
+      lambda=preds*field_fold$time + field_fold$background))
+    if (j %% 20 == 0) {
+      print(paste0("Fold ", i, ": Finished ratio ", j, "/", length(sample_ratios)))
+    }
+  }
+  print(paste0("Fold ", i, ": COMPLETE"))
+}
+
+metrics <- list(crps_pmfp=crps_pmfp, crps_ratio=crps_ratio, post_means=post_means)
+saveRDS(metrics, file=paste0("../results/real_data_cv_metrics",
+  format(Sys.time(), "_%Y%m%d%H%M%S"), ".rds"))
