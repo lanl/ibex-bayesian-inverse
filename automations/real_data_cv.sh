16e3ca31b11d6d8fce3a712710bacc9741dfc736
Steven Barnett <sdbarnett@vt.edu>
2025-09-30 09:44:29 -0400

    passing unique seed

--- automations/real_data_cv.sh
+++ automations/real_data_cv.sh
@@ -20,7 +20,7 @@ seed=$((RANDOM))
 nf="nodes_$(( RANDOM % 9000 + 1000)).txt"
 scontrol show hostname $SLURM_NODELIST > $nf
 
-seq 1 $ncopies | parallel --slf $nf -j$nparallel --wd $PWD --env OMP_NUM_THREADS "module reset; module load R/4.4.2-gfbf-2024a; R CMD BATCH \"--args seed=$seed fold_seed=$((RANDOM)) ncvs=$ncv fid={} nmcmcs=$nmcmc\" real_data_cv.R real_data_cv_{}_$seed.Rout"
+seq 1 $ncopies | parallel --slf $nf -j$nparallel --wd $PWD --env OMP_NUM_THREADS "module reset; module load R/4.4.2-gfbf-2024a; R CMD BATCH \"--args seed=$seed fold_seed={} ncvs=$ncv fid={} nmcmcs=$nmcmc\" real_data_cv.R real_data_cv_{}_$seed.Rout"
 
 # Collect all the results
 R CMD BATCH "--args seed=$seed" real_data_cv_collect.R
