19ea496d7617872886a9dd46098e5ea90f29f873
sdaltonb <sdbarnett@vt.edu>
2025-11-18 16:47:49 -0500

    starting appendix

--- automations/scale_disc_test_collect.R
+++ automations/scale_disc_test_collect.R
@@ -5,5 +5,5 @@ for (i in 1:length(files)) {
   iter_res <- readRDS(paste0("results/", files[i]))
   res[[i]] <- list(logscls=iter_res$mcmc_res$logscl, truth=iter_res$settings$scale)
 }
-saveRDS(metrics, file=paste0("results/scale_disc_test_results",
+saveRDS(res, file=paste0("results/scale_disc_test_results",
   format(Sys.time(), "_%Y%m%d%H%M%S"), ".rds"))
