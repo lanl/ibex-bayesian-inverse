972e965128426987eae23e342cb21c46ed3713b5
sdaltonb <sdbarnett@vt.edu>
2025-11-18 15:34:44 -0500

    code for scale test

--- automations/scale_disc_test.R
+++ automations/scale_disc_test.R
@@ -1,5 +1,3 @@
-
-
 ###############################################################################
 #### Conducts Bayesian Computer Model Calibration for the data collected by
 #### the Interstellar Boundary Explorer satellite. Uses field data from
@@ -8,9 +6,9 @@
 
 source("../helper.R")
 source("../mcmc.R")
+source("../vecchia_scaled.R")
 
 index <- NA
-ncvs <- NA
 scale <- NA
 seed <- 7198701
 map <- "2020A"
@@ -18,7 +16,7 @@ map <- "2020A"
 scales <- c(seq(0.2, 1.0, by=0.2), 1.5, 2, 4, 8, 10)
 
 ## read in the command line arguments
-## run with: R CMD BATCH '--args index=1 scale=0.2 ncvs=10 seed=719 map="2020A"' scale_disc_test.R
+## run with: R CMD BATCH '--args index=1 scale=0.2 seed=719 map="2020A"' scale_disc_test.R
 args <- commandArgs(TRUE)
 if (length(args) > 0) {
   for(i in 1:length(args)) {
@@ -27,37 +25,45 @@ if (length(args) > 0) {
 }
 print(args)
 
-if (!is.na(index) && !is.na(ncvs)) {
-  design <- rep(scales, ncvs)
-  scale <- design[index] 
+if (is.na(index) && is.na(scale)) {
+  stop("must provide a scale or an index")
+} else if (!is.na(index) && is.na(scale)) {
+  scale <- scales[index]
 }
-settings <- data.frame(index=index, ncvs=ncvs, scale=scale, seed=seed)
+settings <- data.frame(index=index, scale=scale, seed=seed)
 print(settings)
 
 ## Load model and field data
 model_data <- read.csv(file="../data/sims.csv")
-sub_model_data <- model_data[model_data$parallel_mean_free_path==1750 & model_data$ratio==0.02,]
+u <- matrix(c(1750, 0.02), nrow=1)
+u_unit <- matrix(c((u[1]-500)/2500, (u[2]-0.001)/(0.1-0.001)), nrow=1)
 field_data <- read.csv(file="../data/ibex_real.csv")
 colnames(field_data)[colnames(field_data)== "counts"] <- "sim_counts"
 colnames(field_data)[colnames(field_data)== "ecliptic_lat"] <- "lat"
 colnames(field_data)[colnames(field_data)== "ecliptic_lon"] <- "lon"
 field_data <- field_data[field_data$map==map,]
 
-field_sim_map <- read.csv(paste0("../data/ibex_sim_map_", map, ".csv"))
-field_sim_map <- field_sim_map[order(field_sim_map$row),]
-fd_true_rate <- scale*sub_model_data[field_sim_map$near_sim,c("blurred_ena_rate")]
-rates_wbg_exp <- (fd_true_rate + field_data$background)*field_data$time
-field_data$sim_counts <- rpois(rates_wbg_exp, rates_wbg_exp)
-
-## Create simulated field data with multiplicative discrepancy
+## Fit Scaled Vecchia GP surrogate
 pd <- preprocess_data(md=model_data, fd=field_data, esa_lev=4,
   fparams=map, scales=c(1, 1), tol=NA, quant=0.0, real=TRUE)
-mcmc_res <- mcmc(Xm=pd$Xmod, Um=pd$Umod, Zm=pd$Zmod, Xf=pd$Xfield,
-  Zf=pd$Zfield, Of=pd$Ofield, m=25, nmcmcs=50, step=0.05,
-  gpmeth="svecchia", vb=TRUE, true_u=NA, true_logscl=NA,
-  betashape=2, adapt=FALSE)
-res <- list(mcmc_res=mcmc_res, settings=settings)
+fit <- fit_scaled(y=pd$Zmod, inputs=as.matrix(cbind(pd$Xmod, pd$Umod)),
+ nug=1e-4, ms=25)
 
-out_fn_sub <- ifelse(!is.na(index), paste0("index_", index), paste0("scale_", scale))
-saveRDS(res, file=paste0("../results/scale_disc_test_", out_fn_sub,
+## Predict at field locations
+XX <- cbind(pd$Xfield, u_unit)
+colnames(XX) <- NULL
+field_data$ena_rate <- predictions_scaled(fit, as.matrix(XX), m=25,
+  joint=FALSE, predvar=FALSE)
+field_data$sim_counts <-
+  rpois((field_data$ena_rate*scale+field_data$background)*field_data$time,
+    (field_data$ena_rate*scale+field_data$background)*field_data$time)
+pd <- preprocess_data(md=model_data, fd=field_data, esa_lev=4,
+  fparams=map, scales=c(1, 1), tol=NA, quant=0.0, real=TRUE)
+
+## Create simulated field data with multiplicative discrepancy
+mcmc_res <- mcmc(Xm=pd$Xmod, Um=pd$Umod, Zm=pd$Zmod, Xf=pd$Xfield,
+  Zf=pd$Zfield, Of=pd$Ofield, m=25, nmcmcs=10000, step=0.05,
+  gpmeth="svecchia", vb=TRUE, true_u=NA, true_logscl=NA, betashape=2)
+res <- list(mcmc_res=mcmc_res, field_data=field_data, settings=settings)
+saveRDS(res, file=paste0("../results/scale_disc_test_scale_", scale,
   format(Sys.time(), "_%Y%m%d%H%M%S"), ".rds"))
--- automations/scale_disc_test.sh
+++ automations/scale_disc_test.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-#SBATCH -N 15
+#SBATCH -N 10
 #SBATCH --ntasks-per-node=8
 #SBATCH -t 72:00:00
 #SBATCH -p normal_q
@@ -13,16 +13,14 @@ module load R/4.4.2-gfbf-2024a
 export OMP_NUM_THREADS=2
 
 map=$1 # map year on which to base simulated field data
-ncv=$2 # number of folds in cross validation
-ncopies=$(($ncv*10))
-nparallel=15
+ncopies=10
 nf="nodes_$(( RANDOM % 9000 + 1000)).txt"
 scontrol show hostname $SLURM_NODELIST > $nf
 
-seq 1 $ncopies | parallel --slf $nf -j$nparallel --wd $PWD --env OMP_NUM_THREADS "module reset; module load R/4.4.2-gfbf-2024a; R CMD BATCH \"--args index={} seed={} ncvs=$ncv map=$map\" scale_disc_test.R scale_disc_test_{}_$seed.Rout"
+seq 1 $ncopies | parallel --slf $nf -j$ncopies --wd $PWD --env OMP_NUM_THREADS "module reset; module load R/4.4.2-gfbf-2024a; R CMD BATCH \"--args index={} seed={} map=$map\" scale_disc_test.R scale_disc_test_{}_$seed.Rout"
 
 # # Collect all the results
-# R CMD BATCH "--args seed=$seed" real_data_cv_collect.R
+R CMD BATCH scale_disc_test_collect.R
 
 # Remove node file
 if [[ -f $nf ]];
