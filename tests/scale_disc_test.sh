507436cf28ad275a731fd9027a1987fcb9bca03c
sdaltonb <sdbarnett@vt.edu>
2025-11-28 13:21:27 -0500

    small edits

--- tests/scale_disc_test.sh
+++ tests/scale_disc_test.sh
@@ -8,14 +8,21 @@
 if ! [[ $1 =~ ^[0-9]+$ ]];
 then
   echo "Argument 1 must be an integer. Indicates the year of satellite data to use."
-  echo "Usage: scale_disc_test.sh [year]"
+  echo "Usage: scale_disc_test.sh [year] [nprocs]"
+  exit 1
+fi
+
+if ! [[ $2 =~ ^[0-9]+$ ]];
+then
+  echo "Argument 2 must be an integer. Indicates the number of parallel processes to run."
+  echo "Usage: scale_disc_test.sh [year] [nprocs]"
   exit 1
 fi
 
 map=$1 # map year on which to base simulated field data
 ncopies=$2 # number of processes to run in parallel
 
-seq 1 $ncopies | parallel -j $ncopies --wd $PWD "R CMD BATCH \"--args index={} seed={} map=$map\" scale_disc_test.R scale_disc_test_{}.Rout"
+seq 1 10 | parallel -j $ncopies --wd $PWD "R CMD BATCH \"--args index={} seed={} map=$map\" scale_disc_test.R scale_disc_test_{}.Rout"
 
 # Collect all the results
 R CMD BATCH scale_disc_test_collect.R
--- tests/scale_disc_test_collect.R
+++ tests/scale_disc_test_collect.R
@@ -5,5 +5,5 @@ for (i in 1:length(files)) {
   iter_res <- readRDS(files[i])
   res[[i]] <- list(logscls=iter_res$res$logscl, truth=iter_res$settings$scale)
 }
-saveRDS(res, file=paste0("scale_disc_test_results",
+saveRDS(res, file=paste0("../papers/scale_disc_test_results",
   format(Sys.time(), "_%Y%m%d%H%M%S"), ".rds"))
