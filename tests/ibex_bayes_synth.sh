36071c9c3a47944573ad33a1624b9e53e670a143
Steven Dalton Barnett - 377859 <sdbarnett@darwin-fe1.lanl.gov>
2026-09-14 16:45:54 -0600

    adding documentation for shell scripts

--- tests/ibex_bayes_synth.sh
+++ tests/ibex_bayes_synth.sh
@@ -1,6 +1,21 @@
 #!/bin/bash
 
+if [ -z "$1" ];
+then
+  echo "Error: missing path to input file"
+  echo "Usage: ibex_bayes_synth.sh [input file path]"
+  exit 1
+fi
+
 infile=$1 # input file with simulator paramters
+
+if [ ! -f "$infile" ];
+then
+  echo "Error: file does not exist"
+  echo "Argument 1 must be a valid file path."
+  echo "Refer to ibex_bayes_test.R for expected file format."
+  exit 1
+fi
 
 echo "Started script"
 R CMD BATCH "--args -v --if=$infile" ibex_bayes_test.R
